482886642d174771a2c71e29e33b8cee0086ed67
Zohaib <zohaibv1999@gmail.com>
2025-06-15 20:33:25 -0400

    fixed comment issue

--- Depth_First_Search/Clone_Graph.py
+++ Depth_First_Search/Clone_Graph.py
@@ -1,12 +1,11 @@
 #https://leetcode.com/problems/clone-graph/description/
 
-"""
-# Definition for a Node.
+
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-"""
+
 
 from typing import Optional
 class Solution:
